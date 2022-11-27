b31ce14158a46eed4947adf16e2a26cd221ee462
JRemedy <JavaRemedy@yandex.ru>
2022-11-27 06:06:14 +0300

    Release first Registration System

--- src/com/dev/Register.java
+++ src/com/dev/Register.java
@@ -1,2 +1,42 @@
-package com.dev;public class Register {
+package com.dev;
+
+public class Register {
+
+    private String username;
+    private String login;
+    private String password;
+    private String email;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
 }
--- src/com/dev/RegistrationForm.java
+++ src/com/dev/RegistrationForm.java
@@ -1,40 +1,77 @@
 package com.dev;
-import java.security.spec.RSAOtherPrimeInfo;
+
 import java.util.Scanner;
 
-public class Registration {
+public class RegistrationForm {
 
     // COLOR CONSOLE TEXT
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
 
-    public static void RegistrationAccount(String[] args) {
+    public static void RegistrationAccount() {
         Register register = new Register();
-        Scanner scanner = new Scanner();
+        Scanner scanner = new Scanner(System.in);
 
         //REGISTRATION USERNAME
-        System.out.println("======ENTER YOUR LOGIN=====");
+        System.out.println("======REGISTER YOUR USERNAME=====");
         String username = scanner.nextLine();
         register.setUsername(username);
 
         //REGISTRATION LOGIN
-        System.out.println("======ENTER YOUR LOGIN=====");
+        System.out.println("======REGISTER YOUR LOGIN=====");
         String login = scanner.nextLine();
         register.setLogin(login);
 
 
-        System.out.println("======ENTER YOUR PASSWORD=====");
+        System.out.println("======REGISTER YOUR PASSWORD=====");
         String password = scanner.nextLine();
         register.setPassword(password);
 
         //REGISTRATION EMAIL
-        System.out.println("======ENTER YOUR EMAIL=====");
+        System.out.println("======REGISTER YOUR EMAIL=====");
         String email = scanner.nextLine();
         register.setEmail(email);
 
-        //REGISTRATION COMPLETE
-        System.out.println(ANSI_GREEN + "=====REGISTRATION COMPLETE=====" + ANSI_RESET);
-        String regComplete = username + login + password + email;
+//        //REGISTRATION COMPLETE
+//        System.out.println(ANSI_GREEN + "=====REGISTRATION COMPLETE=====" + ANSI_RESET);
+//        String regComplete = username + login + password + email;
+//        System.out.print(regComplete);
+
+
+    }
+
+    public static void CaptchaProtect() {
+
+        boolean success = false;
+
+        Scanner scannerCaptcha = new Scanner(System.in);
+
+        do {
+
+            int a = 11111; // генерация числа от
+            int b = 99999; // генерация числа до
+
+            int randomNumber = a + (int) (Math.random() * b); // генерация числа от 11111 до 99999
+            System.out.println("=====ENTER CAPTCHA=====");
+            System.out.println("=====" + "[" + randomNumber + "]" + "=====");
+            int captchaEnter = scannerCaptcha.nextInt();
+
+            success = (captchaEnter == randomNumber);
+
+            if (success) {
+                System.out.print(ANSI_GREEN + "=====CAPTCHA ACCEPT=====" + ANSI_RESET);
+            } else {
+                System.out.println(ANSI_RED + "=====CAPTCHA WRONG!=====" + ANSI_RESET);
+            }
+        } while (!success);
+    }
+
+
+
+    public static void main(String[] args) {
+        RegistrationAccount();
+        CaptchaProtect();
 
 
     }
